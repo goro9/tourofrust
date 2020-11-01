26f0e96cfb5d9536ce62a0c1fa6571a16be7d6bb
goro9 <sokenbiccha@icloud.com>
2020-11-01 20:19:03 +0900

    added chapter4

---
+++ 039.rs
@@ -0,0 +1,17 @@
+fn do_something_that_might_fail(i: i32) -> Result<f32, String> {
+    if i == 42 {
+        Ok(13.0)
+    } else {
+        Err(String::from("this is not the right number"))
+    }
+}
+
+fn main() -> Result<(), String> {
+    let v = do_something_that_might_fail(42).unwrap();
+    println!("found {}", v);
+
+    let v = do_something_that_might_fail(12).unwrap();
+    println!("found {}", v);
+
+    Ok(())
+}
